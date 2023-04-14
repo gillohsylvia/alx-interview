e69392f00ee295d0c1911416fbdc9800e59b2622
gillohsylvia <abdallahfatima363@gmail.com>
2023-04-14 05:29:52 -0700

    Revised

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-from collections import OrderedDict
 
 """
 My pseudocode
@@ -17,55 +16,34 @@ My pseudocode
 * if !key || !value, continue else print status code
 """
 
-def parseLogs():
-
-
-    """
-    Reads logs from standard input and generates reports
-    Reports:
-        * Prints log size after reading every 10 lines 
-        & at KeyboardInterrupt
-    Raises:
-        KeyboardInterrupt (Exception): handles this exception and raises it
-    """
-count = 0
-    global file_size
-    global status_dict
-    file_size = 0
-    status_dict = {200: 0,
-                   301: 0,
-                   400: 0,
-                   401: 0,
-                   403: 0,
-                   404: 0,
-                   405: 0,
-                   500: 0}
-
-    for line in sys.stdin:
-        logs = line.split(" ")
-        if int(logs[-2]) in status_dict.keys() and len(logs) == 9:
-            file_size += int(logs[-1])
-            status_dict[int(logs[-2])] += 1
-        if count % 10 == 0 and count != 0:
-            print_output(status_dict, file_size)
-        count += 1
-
-
-def print_output(status_dict, file_size):
-    """
-    Prints generated report to standard output
-    Args:
-        fileSize (int): This is the total log size after
-        every 10 successfully read line
-        statusCodes (dict): The dictionary of status codes and counts
-    """
-    print("File size: {}".format(file_size))
-    for k, v in status_dict.items():
-        if v > 0:
-            print("{}: {}".format(k, v))
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
-    parse_logs()
-except KeyboardInterrupt:
-    print_output(status_dict, file_size)
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
