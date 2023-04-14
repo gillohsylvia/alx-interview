068bd37f9d3fec9bb8747086139d7e9ee387fb2f
gillohsylvia <abdallahfatima363@gmail.com>
2023-04-14 11:10:39 -0700

    [Feat: task revised]

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,22 +10,42 @@
     step 8 : operation is incremented by i and i is set to 1
     step 9 : return operation when i becomes greater then n
  """
+ 
+def isPrime(n):
+    """
+    Checks if n is a prime number
+    """
 
- def minOperations(n):
-    '''
-    alculates the fewest number of operations needed to
-    result in exactly n H characters
-    '''
-    if n <= 1:  # If n is impossible to achieve
-        return 0
-    num_operations = 0  # number of operations required
-    i = 2  # initialize i to the smallest prime factor
-    # Divide n by its prime factors, starting with the smallest prime factor
-    while i <= n:
+    for i in range(2, (n // 2)):
         if n % i == 0:
-            n //= i
-            num_operations += i
-            i = 2  # reset the value of i
-        else:
-            i += 1
-    return num_operations
+            return False
+    return True
+
+
+def prime_factors(n):
+    """
+    Returns all the prime factors of the number n
+    """
+    factors = []
+    # for i in range(2, (n//2)):
+    for i in range(2, int(n**0.5+1)):
+        while n % i == 0:
+            factors.append(i)
+            n = n // i
+    if n > 1:
+        factors.append(n)
+    return factors
+
+
+def minOperations(n):
+    """
+    Returns an Integer which indicates the fewest number of opertions to get
+    the n of H character in the file given the minimal type of operations
+    allowable
+    """
+    if n <= 1:
+        return 0
+    sum = 0
+    for i in prime_factors(n):
+        sum += i
+    return sum
