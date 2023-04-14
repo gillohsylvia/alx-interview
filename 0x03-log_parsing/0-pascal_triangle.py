6d84c9955ffc16c0487378081535254047410820
gillohsylvia <abdallahfatima363@gmail.com>
2023-04-14 04:34:04 -0700

    Done

---
+++ 0x03-log_parsing/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Generate a pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Generate a pascal triangle
+
+    args:
+        n (int): height of pascal's triangle
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
