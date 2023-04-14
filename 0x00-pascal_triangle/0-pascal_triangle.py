3786886f895ac0a1c56de478a9171b4dfb65f3a9
gillohsylvia <abdallahfatima363@gmail.com>
2023-04-14 05:33:44 -0700

    revised

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""A function that generates a pascal triangle"""
+'''generates pascal's triangle of n rows'''
 
 
 def pascal_triangle(n):
-    """
-    args:
-        n (int): height of pascal's triangle
-    """
-    if n <= 0:
-        return []
-
+    '''generates pascal's triangle of n rows'''
     triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-    return triangle
+    if n <= 0:
+        return triangle
+    else:
+        for i in range(n):
+            row = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            triangle.append(row)
+        return triangle
