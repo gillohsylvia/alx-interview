c109c574a3f3400bbf6cf3a5d54cd133bec711cd
gillohsylvia <abdallahfatima363@gmail.com>
2023-05-17 08:35:43 +0000

    [Feat: correction]

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
 """
-Implement lockboxes
+Contains a function called canUnlockAll
 """
 
 
 def canUnlockAll(boxes):
-
     """
-    Takes a list of lists as an argument and where each box contains the key
-    to the any other box, the first box is unloack by default
+    Determines if all boxes can be opened
     """
+    keys = set()
 
-    obj = {}
-    if len(boxes) <= 0:
-        return False
-    for box in range(len(boxes)):
-        can_unlock = False
-        # print('Box ', box)
-        if box > 0 and box not in obj.keys():
-            for key, value in obj.items():
-                # print(box)
-                # if key == box and value == 'unlocked':
-                if box in boxes[key]:
-                    # print('here')
-                    can_unlock = True
-                    break
-            if not can_unlock:
-                return False
-            continue
-        keys = boxes[box]
-        for key in range(len(keys)):
-            if keys[key] < len(boxes):
-                # checks if key is a valid box index
-                # add the key as the 'key' to the object, obj
-                element = keys[key]
-                obj[element] = 'unlocked'
-            else:
-                obj[element] = 'locked'
-    return True
+    for box_id, box in enumerate(boxes):
+        if len(box) == 0 or box_id == 0:
+            keys.add(box_id)
+        for key in box:
+            if key < len(boxes) and key != box_id:
+                keys.add(key)
+        if len(keys) == len(boxes):
+            return True
+    return False
