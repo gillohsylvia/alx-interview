25f9ab8f539ab34ed4e2335c1bd31ec0cbcfbfe2
gillohsylvia <abdallahfatima363@gmail.com>
2023-05-18 13:34:17 +0000

    revised

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+
+"""
+    A function to determine the fewest number of coins needed
+    to meet a given total
+"""
+
+
+def makeChange(coins, total):
+
+    """
+    This function will take a list of coins and then
+    calculate how much change the total will require
+    """
+
+    if total <= 0:
+        return 0
+
+    else:
+        sort = sorted(coins, reverse=True)
+        counter = 0
+        for i in range(len(sort)):
+            while (total >= sort[i]):
+                counter += 1
+                total -= sort[i]
+    if total == 0:
+        return counter
+    return -1
